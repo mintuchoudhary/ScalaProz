6706952a375a8d8a66cd47d4f69a478fb293ac24
Mintu Choudhary <mintuchoudhary01@gmail.com>
2020-10-09 02:32:10 +0530

    acc

---
+++ src/main/scala/com/m2/AccumulatorDemo.scala
@@ -0,0 +1,18 @@
+import org.apache.spark.sql.SparkSession
+
+/**
+ * Spark Accumulators are shared variables which are only “added” through an associative
+ * and commutative operation and are used to perform counters or sum operations
+ * Written by worker executor node and read by driver
+ */
+object AccumulatorDemo {
+
+  def main(args: Array[String]): Unit = {
+    val sc = SparkSession.builder().appName("AccumulatorDemo").master("local").getOrCreate();
+
+    val accum = sc.sparkContext.longAccumulator("SumAccumulator")
+
+    sc.sparkContext.parallelize(Array(1, 2, 3)).foreach(x => accum.add(x))
+    println("value=" + accum.value)
+  }
+}

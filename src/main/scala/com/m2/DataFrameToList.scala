8465ee7c63a7056cd155c184ea194916d3959fdb
Mintu Choudhary <mintuchoudhary01@gmail.com>
2020-10-10 21:21:05 +0530

    some code

--- src/main/scala/com/m2/DataFrameToList.scala
+++ src/main/scala/com/m2/DataFrameToList.scala
@@ -12,15 +12,29 @@ object DataFrameToList {
     import sparkSession.implicits._
 
     val dataframe = Seq(
-      Array("123", "abc", "2017", "ABC"),
-      Array("456", "def", "2001", "ABC"),
-      Array("789", "ghi", "2017", "DEF")).toDF("col")
+      ("123", "abc", "2017", "ABC"),
+      ("456", "def", "2001", "ABC"),
+      ("789", "ghi", "2017", "DEF")).toDF("c1", "c2", "c3", "c4")
 
+    dataframe.show()
+    /***** List of any 1 cols  *******/
+    //approach 1
+    println(dataframe.select("c1").collect().map(_ (0)).toList) //List(123, 456, 789)
+    //approach 2
+    println(dataframe.select("c1").rdd.map(r => r(0)).collect.toList) //List(123, 456, 789)
 
-    val listValue = dataframe.collectAsList()
-    println(listValue.get(2)) //[WrappedArray(789, ghi, 2017, DEF)]
+    //approach 3 - to get list of String
+    println(dataframe.select("c1").map(r => r.getString(0)).collect.toList) //List(123, 456, 789)
 
-   // println(dataframe.asInstanceOf[List])
+    println(dataframe.select("c1").as[String].collect.toList)
+
+    /***** List of all cols  *******/
+    val listValue = dataframe.collectAsList() //convert entire frame to list
+    println(listValue.get(2)) // [789,ghi,2017,DEF]
+    println(listValue.get(2).getAs("c1")) // 789
+    println(dataframe.select("c1").collectAsList()) //[[123], [456], [789]]
+
+    /***** List of 2 cols  *******/
 
   }
 }

30fb48b8c6e7879b8752df9e9136e8318807ca1b
Mintu Choudhary <mintuchoudhary01@gmail.com>
2020-10-12 01:42:37 +0530

    Create CurryingFunctionDemo.scala

---
+++ src/main/scala/com/m2/CurryingFunctionDemo.scala
@@ -0,0 +1,25 @@
+
+// Scala program add two numbers 
+// using Currying function 
+
+object CurryingFunctionDemo 
+{ 
+
+    def add2(a: Int) = (b: Int) => a + b; 
+
+  
+
+    // Main function 
+
+    def main(args: Array[String]) 
+
+    { 
+
+        // Partially Applied function. 
+
+        val sum = add2(29); 
+
+        println(sum(5)); 
+
+    } 
+} 

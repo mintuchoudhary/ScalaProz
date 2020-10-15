406927fa9b0ad86807b222f539dc220e8ca37c1b
Mintu Choudhary <mintuchoudhary01@gmail.com>
2020-10-16 00:50:27 +0530

    Add files via upload

---
+++ src/main/scala/com/m2/RDDAddColumn.scala
@@ -0,0 +1,65 @@
+import org.apache.spark.sql.SparkSession
+import org.apache.spark.sql.functions._
+
+/**
+ * Add a column to RDD,
+ * Convert RDD to DataFrame, and add new column
+ */
+object RDDAddColumn {
+  def main(args: Array[String]): Unit = {
+
+    val spark = SparkSession.builder().appName("AccumulatorDemo").master("local").getOrCreate()
+    spark.sparkContext.setLogLevel("ERROR")
+    val fileRDD = spark.sparkContext.textFile("src/main/resources/data")
+    fileRDD.foreach(println)
+
+    val newRDD = fileRDD.map(lines => {
+      val fields = lines.split(",")
+      if (fields(1).toInt > 18)
+        (fields(0), fields(1), fields(2), "Y")
+      else
+        (fields(0), fields(1), fields(2), "N")
+    })
+    newRDD.foreach(println)
+
+    /** ***********Convert RDD to DataFrame ****************/
+    import spark.implicits._
+    val dataDF = fileRDD.
+      map(line => {
+        val fields = line.split(",")
+        (fields(0), fields(1), fields(2))
+      }).
+      toDF("name", "age", "city")
+    dataDF.show(false)
+
+    dataDF.withColumn("eligible", when(col("age") > 18, "Y").otherwise("N")).show()
+  }
+}
+
+/**
+ * Mintu,27,Pune
+ * Meg,25,PUN
+ * SS,26,Mum
+ * DD,17,Chennai
+ * (Mintu,27,Pune,Y)
+ * (Meg,25,PUN,Y)
+ * (SS,26,Mum,Y)
+ * (DD,17,Chennai,N)
+ * +-----+---+-------+
+ * |name |age|city   |
+ * +-----+---+-------+
+ * |Mintu|27 |Pune   |
+ * |Meg  |25 |PUN    |
+ * |SS   |26 |Mum    |
+ * |DD   |17 |Chennai|
+ * +-----+---+-------+
+ * *
+ * +-----+---+-------+--------+
+ * | name|age|   city|eligible|
+ * +-----+---+-------+--------+
+ * |Mintu| 27|   Pune|       Y|
+ * |  Meg| 25|    PUN|       Y|
+ * |   SS| 26|    Mum|       Y|
+ * |   DD| 17|Chennai|       N|
+ * +-----+---+-------+--------+
+ */

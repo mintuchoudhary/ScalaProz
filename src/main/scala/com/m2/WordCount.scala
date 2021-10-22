5b13f5e43ffb6ae49fd8c2b8da5bab05bac8aa36
Mintu Choudhary <mintu_choudhary01@yahoo.in>
2021-10-23 00:35:32 +0530

    letter /word count

--- src/main/scala/com/m2/WordCount.scala
+++ src/main/scala/com/m2/WordCount.scala
@@ -1,27 +1,33 @@
 package com.m2
 
 import org.apache.spark.sql.SparkSession
+import org.apache.spark.sql.functions._
 
 object WordCount {
   System.setProperty("hadoop.home.dir", "D:\\Downloads\\hadoop") //for bin/winutils.exe
 
   def main(args: Array[String]) {
     // val spark = SparkSession.builder().master("local").getOrCreate()
-    val sparkSession = SparkSession.builder().appName("word count").master("local").getOrCreate();
-      sparkSession.sparkContext.setLogLevel("WARN")
+    val sparkSession = SparkSession.builder().appName("word count")
+      //.config("spark.testing.memory", "2147480000")
+      .master("local").getOrCreate();
+    sparkSession.sparkContext.setLogLevel("WARN")
     import sparkSession.implicits._
 
     // val inputData1 = sparkSession.read.option("header", true).option("delimiter", ",").csv("data.txt")
     val inputData1 = sparkSession.sparkContext.textFile("src/main/resources/data.txt")
     println(inputData1.toDF().show(false))
 
-    val wordCountDF = inputData1.flatMap(line => line.split(" ")).map(word => (word, 1)).reduceByKey(_ + _)
+    val wordCountRDD = inputData1.flatMap(line => line.split(" ")).map(word => (word, 1)).reduceByKey(_ + _)
+    wordCountRDD.toDF("Word", "Count").show(false)
 
-   // print(wordCountDF.toDF().show(false))
-  //  print(wordCountDF.count)
-    wordCountDF.foreach(x => x._1)
-    println("end")
-    // sparkSession.stop()
+    val letterCountRDD = inputData1.flatMap(line => line.split("")).map(word => (word, 1)).reduceByKey(_ + _)
+    letterCountRDD.toDF("Letter", "Count")
+      //.filter(col("Letter") <=> " ")
+      .show(50, false)
+
+
+    sparkSession.stop()
   }
 
 }
